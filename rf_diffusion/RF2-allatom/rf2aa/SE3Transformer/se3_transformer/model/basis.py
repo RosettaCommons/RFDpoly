87513554a214bb70915d8de489f82fd9a5be3b9f
Hope Woods <woodsh@louis.graylab.jhu.edu>
2026-02-02 12:39:46 -0500

    Adding util function to avoid NVTX crashes on CPU

--- rf_diffusion/RF2-allatom/rf2aa/SE3Transformer/se3_transformer/model/basis.py
+++ rf_diffusion/RF2-allatom/rf2aa/SE3Transformer/se3_transformer/model/basis.py
@@ -29,25 +29,10 @@ import e3nn.o3 as o3
 import torch
 import torch.nn.functional as F
 from torch import Tensor
-from torch.cuda.nvtx import range as nvtx_range
+from se3_transformer.utils.nvtx import nvtx_range
 
 from se3_transformer.runtime.utils import degree_to_dim
 
-from contextlib import contextmanager
-
-@contextmanager
-def safe_nvtx_range(msg):
-    try:
-        import torch
-        if torch.cuda.is_available():
-            from torch.cuda.nvtx import range as nvtx_range
-            with nvtx_range(msg):
-                yield
-        else:
-            yield
-    except Exception:
-        # NVTX missing or broken → just run the code
-        yield
 
 @lru_cache(maxsize=None)
 def get_clebsch_gordon(J: int, d_in: int, d_out: int, device) -> Tensor:
@@ -69,7 +54,7 @@ def get_all_clebsch_gordon(max_degree: int, device) -> List[List[Tensor]]:
 
 def get_spherical_harmonics(relative_pos: Tensor, max_degree: int) -> List[Tensor]:
     all_degrees = list(range(2 * max_degree + 1))
-    with safe_nvtx_range('spherical harmonics'):
+    with nvtx_range('spherical harmonics'):
         sh = o3.spherical_harmonics(all_degrees, relative_pos, normalize=True)
         return torch.split(sh, [degree_to_dim(d) for d in all_degrees], dim=1)
 
@@ -178,13 +163,13 @@ def get_basis(relative_pos: Tensor,
               compute_gradients: bool = False,
               use_pad_trick: bool = False,
               amp: bool = False) -> Dict[str, Tensor]:
-    with safe_nvtx_range('spherical harmonics'):
+    with nvtx_range('spherical harmonics'):
         spherical_harmonics = get_spherical_harmonics(relative_pos, max_degree)
-    with safe_nvtx_range('CB coefficients'):
+    with nvtx_range('CB coefficients'):
         clebsch_gordon = get_all_clebsch_gordon(max_degree, relative_pos.device)
 
     with torch.autograd.set_grad_enabled(compute_gradients):
-        with safe_nvtx_range('bases'):
+        with nvtx_range('bases'):
             basis = get_basis_script(max_degree=max_degree,
                                      use_pad_trick=use_pad_trick,
                                      spherical_harmonics=spherical_harmonics,
